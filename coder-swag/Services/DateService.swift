fc08bd74bad847f47c812368af66756b85d0698f
MatanHu <MatanHuja@gmail.com>
2018-09-10 03:09:00 +0300

    initial categories with data

---
+++ coder-swag/Services/DateService.swift
@@ -0,0 +1,23 @@
+//
+//  DateService.swift
+//  coder-swag
+//
+//  Created by MatanHuja on 10/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import Foundation
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
