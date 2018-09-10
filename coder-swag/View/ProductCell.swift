7cdd3a84497d7c02ae4d83d70d351e9da2290648
MatanHu <MatanHuja@gmail.com>
2018-09-10 03:31:58 +0300

    start create Product page

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by MatanHuja on 10/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
